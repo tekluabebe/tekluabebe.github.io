a20c167d9222985fbc9f75a7ff1de65a8077cd7b
tekluabebe <88448375+tekluabebe@users.noreply.github.com>
2024-11-07 10:31:01 +0300

    Add files via upload

---
+++ indent.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+	<title>riftvalley university</title>
+	<link href="https://fonts.googleapis.com/css?family=Lobster|Montserrat" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="styleindex.css">
+</head>
+<body>
+	<header>
+		<nav>
+			<h1>riftvalley univ.</h1>
+			<ul id="navli">
+				<li><a class="homered" href="indent.php">HOME</a></li>
+				<li><a class="homeblack" href="aboutus.html">ABOUT US</a></li>
+				<li><a class="homeblack" href="contact.html">CONTACT</a></li>
+				<li><a class="homeblack" href="elogin.html">LOG IN</a></li>
+			</ul>
+		</nav>
+	</header>
+	
+	<div class="divider"></div>
+	<div id="divimg">
+		
+	</div>
+
+	
+	<p style="text-align: center;"><img src="process/images/dd1.png" alt="Avatar" style="width:850px"></p>
+
+	
+		
+		<h1 style= "color: rgb(0, 255, 76); font-family: 'Lobster', cursive; font-weight: 200; font-size: 50px; margin-top: 100px; text-align: center;">welcome to riftvalley university employee management system.</h1>
+		
+
+		
+
+	
+</body>
+</html>
---
+++ indentt.php
@@ -0,0 +1,32 @@
+<table>
+
+			<tr bgcolor="#000">
+				<th align = "center">Seq.</th>
+				<th align = "center">Emp. ID</th>
+				<th align = "center">Name</th>
+				<th align = "center">Points</th>
+				
+
+			</tr>
+
+			
+
+			<?php
+				$seq = 1;
+				while ($employee = mysqli_fetch_assoc($result)) {
+					echo "<tr>";
+					echo "<td>".$seq."</td>";
+					echo "<td>".$employee['id']."</td>";
+					
+					echo "<td>".$employee['firstName']." ".$employee['lastName']."</td>";
+					
+					echo "<td>".$employee['points']."</td>";
+					
+					$seq+=1;
+				}
+
+
+			?>
+
+		</table>
+        <h2 style="font-family: 'Montserrat', sans-serif; font-size: 25px; text-align: center;">Empolyee Leaderboard </h2>
